0179f1961378cc999da3ca4f316b3fe61bc8adec
avtoberaia <beraiaatuka@gmail.com>
2024-09-13 19:16:37 +0400

    commit

---
+++ level 18/homework/homework.py
@@ -0,0 +1,3 @@
+#or operatori aris magalitad true or false aris false da false or false aris false
+#and operaroti aris magalitad  true and false aris false da false and false aris false
+
---
+++ level 18/homework/homework3.py
@@ -0,0 +1,20 @@
+print(true or true) #true 
+print(false or true) #true
+print(false or false) #false
+print(true or true )  #true              
+print(true or false)  #true
+print(true or false )  #true
+print(false or false)  # false 
+print(true or false)  #true
+print(Talse or true)  #true
+print(True or true)   #true
+print(True and false ) #false
+print(True and true)   #true 
+print(false and false) #false
+print(true and true)   #true
+print(false and false )  #false
+print(true and false)    #false
+print(true and true)      #true
+print(false and false)   #false
+print(true and false )  #false
+print(true and false)  #false
